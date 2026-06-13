db3a507af9ef349a9b710001649720fde33daebf
Lucas Schoenfelder <lucaseschoenfelder@hotmail.com>
2026-06-13 18:08:57 -0300

    added solution for rotting_oranges

--- neetcode_workspace/rotting_oranges/solution.py
+++ neetcode_workspace/rotting_oranges/solution.py
@@ -2,5 +2,37 @@ class Solution:
     # Time: O(?)
     # Space: O(?)
     def oranges_rotting(self, grid: list[list[int]]) -> int:
-        # TODO: Implement oranges_rotting
-        return 0
+        def has_fresh():
+            return any(1 in row for row in grid)
+        
+        
+        ROWS, COLS = len(grid), len(grid[0])
+        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+        t = 0
+        
+        changed = True
+        while has_fresh() and changed:
+            changed = False
+            
+            for row in range(ROWS):
+                for col in range(COLS):
+                    if grid[row][col] == 2:
+                        
+                        for dr, dc in directions:
+                            r, c = row + dr, col + dc
+                            
+                            if r in range(ROWS) and c in range(COLS) and grid[r][c] == 1:
+                                grid[r][c] = 3
+                                changed = True
+            
+            if not changed:
+                return -1
+            
+            for row in range(ROWS):
+                for col in range(COLS):
+                    if grid[row][col] == 3:
+                        grid[row][col] = 2
+            
+            t += 1
+        
+        return t if not has_fresh() else -1
